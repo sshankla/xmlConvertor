167ea38d441543ee7deeb1da92335497112e3b6f
sshankla <sshankla@gmail.com>
2019-01-18 17:52:29 +1100

    Started coding as per New Architecture to make it generic v1.1

--- convertor/convertor.py
+++ convertor/convertor.py
@@ -1,12 +1,18 @@
 #/bash/python
 # JDS Python Script for converting Geo CSV to XML
-# Author: Suraj Shankla (suraj.shankla@jds.net.au) 
+# Author: Suraj Shankla (suraj.shankla@jds.net.au)
+# Revised on 18th Jan, 2018. 
 import xml.etree.ElementTree as ET
 import os
 import copy
+
 '''sample.xml template for target xml
 ld --> list of dictionary, each dictionary having
 values to be populated'''
+
+def readmappingconf(path):
+        return open(path + '/convertor/mapping.conf','r')
+
 
 def readtemplate(xml):
     tree = ET.parse(xml)
@@ -37,10 +43,43 @@ def createlistofxmls(ld,sample_tree):
 
     return target_list
 
+'''Initiate accomplishes following:
+    1. Finds the number of Keys for each element in ld_values
+    2. Initiates a mapping of Keys and Columns based on mapping.conf'''
+
+def initiate():
+        mcf = readmappingconf(os.getcwd())
+
+        value_column_mapping = {}
+        number_of_mappings = 0
+        for line in mcf:
+                tokenlist = []
+                tokenlist = line.split('=')
+                if line[0] == 'V':
+                        # Using -2 because -1 is carriage return '\n'
+                        value_column_mapping[tokenlist[0]] = int(line[-2])-1 
+                        number_of_mappings = number_of_mappings + 1
+        
+        config = (number_of_mappings,value_column_mapping)
+        return config
 
 
-'''ld_values is list of dictionary
-where each entry is a dictionary of values'''
+
+'''Step 1: ld_values is list of dictionary
+where each entry is a dictionary of values.
+#############################################################
+1. Change dated 18th Jan, 2018. 
+Will Create a dictionary of values dynamically based number
+of values specified in mapping.conf. 
+ncreatelistofdict (list_of_dictionary,csvfile):list
+#############################################################'''
+
+def n_createlistofdict(ld_values,csvfile):
+
+        return 1
+
+
+
 
 def createlistofdict(ld_Values,csvfile):
         "List of all Lines"
@@ -73,14 +112,17 @@ def spoolvalues(ld,spoolfile):
         return f
 
 if __name__=='__main__':
+    '''t = initiate()
+    print(t[0],t[1])'''
     path=os.getcwd()
     sld=[]
+    readmappingconf()
     tree = readtemplate(path+'/sample.xml')
     csvfile=open(path+'/sample.data','r')
     sld=createlistofdict(sld,csvfile)
     tld=createlistofxmls(sld,tree)
     f=spoolvalues(tld,path+'/spool.data')
-    f.close()
+    f.close()'''
 
 
 
